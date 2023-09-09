488ba546db1591d42dee16a5db739577591fd018
go75 <2548604505@qq.com>
2023-09-09 11:34:41 +0800

    resume update

--- blog.sh
+++ blog.sh
@@ -12,7 +12,7 @@ dfs(){
             dfs $1/$file
             s=$s'</blockquote>\n'
         else
-            src=blog#${1#???????}/$file
+            src=https://go75.github.io/blog#${1#???????}/$file
             s=$s"<a href=\"$src\">$file</a><br>"
         fi
     done
